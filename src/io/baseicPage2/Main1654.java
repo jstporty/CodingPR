3eb4c09c01f93f67b3f2e6a4ae05bf3d4bcf6565
승현 <horang@mz.co.kr>
2024-04-01 13:18:48 +0900

    이것저것

---
+++ src/io/baseicPage2/Main1654.java
@@ -0,0 +1,19 @@
+//package io.baseicPage2;
+//
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.nio.Buffer;
+//import java.util.Arrays;
+//
+//public class Main1654 {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int n = Integer.parseInt(br.readLine());
+//        int[] arr = new int[n];
+//
+//        for (int i = 0; i < n; i++) {
+//
+//        }
+//    }
+//}
