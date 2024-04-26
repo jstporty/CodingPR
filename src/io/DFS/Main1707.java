725934b2611caed3215e43f6d3fcb730049a2375
승현 <horang@mz.co.kr>
2024-04-26 17:57:13 +0900

    dfs bfs(WPI)

--- src/io/DFS/Main1707.java
+++ src/io/DFS/Main1707.java
@@ -13,22 +13,23 @@ public class Main1707 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int num = Integer.parseInt(br.readLine());
+
         for (int i = 0; i < num; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             node = Integer.parseInt(st.nextToken());
             line = Integer.parseInt(st.nextToken());
             arr = new int[node + 1][node + 1];
             visted = new boolean[node + 1];
-
+            int count = 0;
             for (int j = 1; j < line + 1; j++) {
                 StringTokenizer st1 = new StringTokenizer(br.readLine());
                 int a = Integer.parseInt(st1.nextToken());
                 int b = Integer.parseInt(st1.nextToken());
-                arr[a][b] = arr[b][a] = 1;
+                arr[a][b] =  1;
             }
-            int count = 0;
+
+
             for (int k = 1; k <= node; k++) {
                 if (!visted[k]) {
                     dfs(k);
@@ -37,12 +38,10 @@ public class Main1707 {
             }
 
             if (count > 1) {
-                System.out.println("NO ");
-            } else System.out.println("YES");
+                System.out.println("YES ");
+            } else System.out.println("NO");
             count = 0;
         }
-
-
     }
 
     public static void dfs(int param) {
